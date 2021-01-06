7a584d0b3bfb35a2d4e67f3b5389ec6d5e1c683e
tsendsuren8 <72597028+tsendsuren8@users.noreply.github.com>
2021-01-06 15:24:16 +0800

    up

--- myinit.sh
+++ myinit.sh
@@ -2,3 +2,8 @@ sudo apt install -y git
 sudo apt install -y python3-pip
 pip3 install pillow
 pip3 install scrapy
+wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
+sudo apt install -y apt-transport-https
+echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
+sudo apt update
+sudo apt install -y sublime-text
